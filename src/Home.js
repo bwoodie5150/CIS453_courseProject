d6da568dbd66d374b8bd2dcffc5c3d76b4e3163c
bwoodie5150 <75141041+bwoodie5150@users.noreply.github.com>
2020-12-08 16:37:30 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,11 @@ class Home extends Component
 {
     render() {
         return (
-            <div>
+          <div id="text">
                 <h2>Welcome to MGBD Parts & Service</h2> 
-                <figure>
-                <img src="https://www.telegraph.co.uk/multimedia/archive/01808/Rover-P6_1808983c.jpg" alt="" style="width:100%">
-                <figcaption>Mark & Angie enjoying their Rover P6</figcaption></img>
-                </figure>
+                <div id="home">
+                <h1>Your Source for Classic car parts since 2010</h1>
+                </div>
             </div>
         )
     }
