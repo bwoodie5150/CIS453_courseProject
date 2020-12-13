6d2969bd6901e05e144bfe458010920ad0dad23a
bwoodie5150 <you@example.com>
2020-12-13 17:15:11 -0500

    Final Revision

--- src/Parts.js
+++ src/Parts.js
@@ -8,12 +8,20 @@ class Parts extends Component
                    <h2>Welcome to our parts warehouse</h2> 
                 <ul>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Bearings"><li>Bearings</li></a>
+                    <img src="https://base.imgix.net/files/base/ebm/machinedesign/image/2019/04/machinedesign_11817_ballbearings_500144555_0.png?auto=format&fit=max&w=1200" alt=""></img>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Body__Trim"><li>Body & Trim</li></a>
+                    <img src="https://www.stoddard.com/media/catalog/category/exterior_trim.png" alt=""></img>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Category2"><li>Braking</li></a>
+                    <img src="https://i.pinimg.com/originals/e1/97/c4/e197c43609f9d8dd6f1e9652f25bc398.jpg" alt=""></img>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Clutch"><li>Clutch</li></a>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy-Ak7eZSE0vt9pKPYIh5Zg19BarCKLACuhA&usqp=CAU" alt=""></img>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Electrical"><li>Electrical</li></a>
+                    <img src="https://d32ptomnhiuevv.cloudfront.net/en-us/sites/default/files/styles/blog_landing_header_687/public/How_car_electrical_system_works.png?itok=CH-sqFy0" alt=""></img>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Engine_4_Cyl"><li>Engine 4 Cyl</li></a>
+                    <img src="https://cdn3.volusion.com/rnmwa.rjxsk/v/vspfiles/photos/M-6010-23T-2T.jpg" alt=""></img>
                     <a href="http://shop.roverp6cars.com/epages/es146747.sf/en_GB/?ObjectPath=/Shops/es146747/Categories/Engine_8_Cyl"><li>Engine 8 Cyl</li></a>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP2902LdNM0xTob7rP8acfR9JPPYlP94bN1A&usqp=CAU" alt=""></img>
+
                 </ul>
             </div>
         )
